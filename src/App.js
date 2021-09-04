a62102a1d018b24e0e6b689a84846a9739aea5c6
sabdulmajid <ayman.hasib@outlook.com>
2021-09-04 21:11:53 +0300

    Finishing touches & deploying with to Firebase

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import './App.css';
+import "./App.css";
 import TextField from "@material-ui/core/TextField";
-import { useEffect, useState } from 'react';
-import { Button } from '@material-ui/core';
-import { db } from './firebase_config';
+import { useEffect, useState } from "react";
+import { Button } from "@material-ui/core";
+import { db } from "./firebase_config";
 import firebase from "firebase";
+import TodoListItem from "./List";
 
 function App() {
   const [todos, setTodos] = useState([]);
@@ -12,7 +13,7 @@ function App() {
 
   useEffect(() => {
     getTodos();
-  }, []) // Keeping the TextFiled blank ONLY on start of the app
+  }, []); // Keeping the TextField blank ONLY on start of the app
 
   function getTodos() {
     db.collection("todos").onSnapshot(function (querySnapshot) {
@@ -20,14 +21,14 @@ function App() {
         querySnapshot.docs.map((doc) => ({
           id: doc.id,
           todo: doc.data().todo,
-          isComplete:doc.data().isComplete
+          isComplete: doc.data().isComplete,
         }))
       );
-    })
+    });
   }
 
-  function addTodo (e) {
-    e.preventDefault();
+  function addTodo(event) {
+    event.preventDefault();
 
     db.collection("todos").add({
       isComplete: true,
@@ -36,23 +37,50 @@ function App() {
     });
 
     setTodoInput("");
-
   }
 
   return (
-    <div className="App" style={{}}>
-      <h1>Sanskar & Ayman's To Do App</h1>
-      <form>
-      <TextField 
-        id = "filled-basic" 
-        label = "Write a task"
-        variant = "filled"
-        value = {todoInput} 
-        onChange = {(e) => setTodoInput(e.target.value)}
-      />
-      <Button type="submit" variant="contained" color="primary" disableElevation onClick={addTodo}>
-        Primary
-      </Button></form>
+    <div className="App">
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+          width: "100%",
+        }}
+      >
+        <h1>Ayman & Sanskar's To Do App</h1>
+        <form>
+          <TextField
+            id="filled-basic"
+            label="Write a task"
+            variant="filled"
+            value={todoInput}
+            style={{ width: "90vw", maxWidth: "500px" }}
+            onChange={(event) => setTodoInput(event.target.value)}
+          />
+          <Button
+            type="submit"
+            variant="contained"
+            color="primary"
+            onClick={addTodo}
+            style={{ display: "none" }}
+          >
+            Primary
+          </Button>
+        </form>
+        
+        <div style={{ width: "90vw", maxWidth: "500px", marginTop: "25px" }}>
+          {todos.map((todo) => (
+            <TodoListItem
+              todo={todo.todo}
+              isComplete={todo.isComplete}
+              id={todo.id}
+            />
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
--- src/firebase_config.js
+++ src/firebase_config.js
@@ -1,12 +1,12 @@
 import firebase from "firebase";
 
 var firebaseConfig = {
-    apiKey: "AIzaSyBSi-BY7kVzhKkBxTIVPJRJLzyI0Y4J-i0",
-    authDomain: "teacher-s-hack-project.firebaseapp.com",
-    projectId: "teacher-s-hack-project",
-    storageBucket: "teacher-s-hack-project.appspot.com",
-    messagingSenderId: "598750062086",
-    appId: "1:598750062086:web:d69f7ba095f39b03898587"
+  apiKey: "AIzaSyBSi-BY7kVzhKkBxTIVPJRJLzyI0Y4J-i0",
+  authDomain: "teacher-s-hack-project.firebaseapp.com",
+  projectId: "teacher-s-hack-project",
+  storageBucket: "teacher-s-hack-project.appspot.com",
+  messagingSenderId: "598750062086",
+  appId: "1:598750062086:web:d69f7ba095f39b03898587",
 };
 
 firebase.initializeApp(firebaseConfig);
